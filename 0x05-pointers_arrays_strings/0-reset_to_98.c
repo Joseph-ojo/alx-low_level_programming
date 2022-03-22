81eceb186b51c6ff1ed495247bd3ab9857031068
Ojo Joseph <40062472+Joseph-ojo@users.noreply.github.com>
2022-03-22 02:01:03 +0100

    Create 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+*  reset_to_98 - A function that takes a pointer to an
+* int as parameter and updates the value it points to to 98.
+*@n: pointer to the int to be modified
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+© 2022 GitHub, Inc.
+Terms
